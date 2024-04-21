9d4fda30d78fa54ad4a1e2a974c60e905ccdf8de
vquang <nguyenvinhquang19a1@gmail.com>
2024-04-21 18:34:34 +0700

    notification appointment

--- app/src/main/java/com/example/medication/vinhquang/util/GlobalValues.java
+++ app/src/main/java/com/example/medication/vinhquang/util/GlobalValues.java
@@ -1,6 +1,7 @@
 package com.example.medication.vinhquang.util;
 
 import com.example.medication.vinhquang.data.NotificationFirebase;
+import com.example.medication.vinhquang.data.SearchResponse;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,8 +9,12 @@ import java.util.List;
 public class GlobalValues {
     private static GlobalValues instance;
     private String token;
-    private Integer userId = 1;
+    private Integer userId;
+    private Integer role;
     private List<NotificationFirebase> notificationList = new ArrayList<>();
+    private Integer searchType;
+    private SearchResponse docterSearch;
+    private SearchResponse patientSearch;
 
     private GlobalValues() {
         // Khởi tạo các giá trị mặc định
@@ -48,5 +53,37 @@ public class GlobalValues {
 
     public void setNotificationList(List<NotificationFirebase> notificationList) {
         this.notificationList = notificationList;
+    }
+
+    public Integer getSearchType() {
+        return searchType;
+    }
+
+    public void setSearchType(Integer searchType) {
+        this.searchType = searchType;
+    }
+
+    public SearchResponse getDocterSearch() {
+        return docterSearch;
+    }
+
+    public void setDocterSearch(SearchResponse docterSearch) {
+        this.docterSearch = docterSearch;
+    }
+
+    public SearchResponse getPatientSearch() {
+        return patientSearch;
+    }
+
+    public void setPatientSearch(SearchResponse patientSearch) {
+        this.patientSearch = patientSearch;
+    }
+
+    public Integer getRole() {
+        return role;
+    }
+
+    public void setRole(Integer role) {
+        this.role = role;
     }
 }
